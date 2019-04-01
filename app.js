8c4cb41a2e3dd6ba1065f88516a40a219bb68030
Ali Kurtulush <alikurtulus-92@hotmail.com>
2019-04-01 15:33:06 +0100

    Added ghost moving

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
   let foodCount = 0
   let ghostCount = 0
   let keyVal = 0
-  let timerId =0
+  let timerId = 0
   const squares = []
   let scoreResult = 0
   let playerIndex = 0
-
+  let playerMove = 1
+  const ghostPositions = []
   let direction = 'forward'
   let currentStep = 0
+  let ghostStep =0
+  let ghostStep2 =0
+  let ghostI = 0
   const mazeArray = [1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
     1, 0, 0, 1, 0, 1, 0, 0, 0, 1,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -52,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         console.log(randCherry)
 
-        if (squares[randCherry].classList.contains('way') && !squares[randCherry].classList.contains('player') && randCherry!==0) {
+        if (squares[randCherry].classList.contains('way') && !squares[randCherry].classList.contains('player') && randCherry !== 0) {
 
           squares[randCherry].classList.add('cherry')
 
@@ -63,29 +67,142 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
   }
+
   function createGhosts() {
-    while (ghostCount < 4) {
-      for (let i = 0; i < 5; i++) {
+    while (ghostCount < 2) {
+      for (let i = 0; i < 1; i++) {
         const randGhost = parseInt(Math.floor(Math.random() * 99))
         console.log(ghostCount)
 
         console.log(ghostCount)
 
-        if (squares[randGhost].classList.contains('way') && !squares[randGhost].classList.contains('player') && !squares[randGhost].classList.contains('cherry')) {
+        if (squares[randGhost].classList.contains('way') && !squares[randGhost].classList.contains('player') && !squares[randGhost].classList.contains('cherry') && randGhost !== 0) {
 
           squares[randGhost].classList.add('ghost')
+
+          ghostPositions.push(randGhost)
 
           ghostCount++
         }
 
       }
     }
+    ghostStep = ghostPositions[0]
+    ghostStep2 = ghostPositions[1]
 
   }
 
   createMaze()
   createFood()
   createGhosts()
+
+  function moveGhosts() {
+
+  }
+  const timerGhostId = setInterval(() => {
+
+    const availableMoves = [1, -1, width, -width]
+
+    let val = availableMoves[Math.floor(Math.random() * availableMoves.length)]
+    let val2 = availableMoves[Math.floor(Math.random() * availableMoves.length)]
+
+    while (squares[ghostStep + val].classList.contains('wall') ||  squares[ghostStep + val].classList.contains('cherry')) {
+      val = availableMoves[Math.floor(Math.random() * availableMoves.length)]
+    }
+    while (squares[ghostStep2 + val2].classList.contains('wall') ||  squares[ghostStep2 + val2].classList.contains('cherry')) {
+      val2= availableMoves[Math.floor(Math.random() * availableMoves.length)]
+    }
+
+    squares[ghostStep].classList.remove('ghost')
+    squares[ghostStep2].classList.remove('ghost')
+
+    console.log(ghostStep)
+    if (ghostStep < 99) {
+      if (val === -1) {
+        if (squares[ghostStep - 1].classList.contains('way')) {
+
+
+          ghostStep -= 1
+
+          console.log(ghostStep)
+
+          squares[ghostStep].classList.add('ghost')
+        }
+      } else if (val === width) {
+        if (squares[ghostStep + width].classList.contains('way')) {
+          console.log('hi down')
+          ghostStep +=width
+          squares[ghostStep].classList.add('ghost')
+
+
+        }
+      } else if (val === 1) {
+        if (squares[ghostStep + 1].classList.contains('way')) {
+          console.log('hi forward')
+          ghostStep += 1
+          squares[ghostStep].classList.add('ghost')
+
+
+        }
+      } else if (val === -width) {
+        if (squares[ghostStep - width].classList.contains('way')) {
+          console.log('hi up')
+          ghostStep -= width
+          squares[ghostStep].classList.add('ghost')
+
+        }
+      }
+    } else {
+      ghostStep -= 1
+      squares[ghostStep].classList.add('ghost')
+    }
+    if (ghostStep2 < 99) {
+      if (val2 === -1) {
+        if (squares[ghostStep2 - 1].classList.contains('way')) {
+
+
+          ghostStep2 -= 1
+
+          console.log(ghostStep2)
+
+          squares[ghostStep2].classList.add('ghost')
+        }
+      } else if (val2=== width) {
+        if (squares[ghostStep2 + width].classList.contains('way')) {
+          console.log('hi down')
+          ghostStep2 +=width
+          squares[ghostStep2].classList.add('ghost')
+
+
+        }
+      } else if (val2 === 1) {
+        if (squares[ghostStep2 + 1].classList.contains('way')) {
+          console.log('hi forward')
+          ghostStep2 += 1
+          squares[ghostStep2].classList.add('ghost')
+
+
+        }
+      } else if (val2 === -width) {
+        if (squares[ghostStep2 - width].classList.contains('way')) {
+          console.log('hi up')
+          ghostStep2 -= width
+          squares[ghostStep2].classList.add('ghost')
+
+        }
+      }
+    } else {
+      ghostStep2 -= 1
+      squares[ghostStep2].classList.add('ghost')
+    }
+
+
+
+
+
+
+  }, 1000)
+
 
 
   squares[playerIndex].classList.add('player')
@@ -141,12 +258,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  function timer(){
+  function timer() {
     clearInterval(timerId)
     timerId = setInterval(() => {
       timeResult--
       time.innerText = timeResult
-      if(foodCount===0)
+      if (foodCount === 0)
         createFood()
 
       if (timeResult === 0) {
@@ -160,16 +277,16 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  startButton.addEventListener('click',timer)
+  startButton.addEventListener('click', timer)
   if (timeResult === 0) {
     clearInterval(timerId)
     return
   }
-  resetButton.addEventListener('click',()=>{
-    score.innerText=0
-    scoreResult=0
-    timeResult=60
-    time.innerText=60
+  resetButton.addEventListener('click', () => {
+    score.innerText = 0
+    scoreResult = 0
+    timeResult = 60
+    time.innerText = 60
 
     createFood()
     createGhosts()
@@ -185,7 +302,7 @@ document.addEventListener('DOMContentLoaded', () => {
         keyVal = 37
         if (playerIndex % width > 0) {
           playerIndex--
-
+          playerMove = playerMove - 1
           direction = 'backward'
           move()
         }
@@ -196,7 +313,8 @@ document.addEventListener('DOMContentLoaded', () => {
         keyVal = 38
         if (playerIndex - width >= 0) {
           playerIndex -= width
-
+          playerMove = playerMove + width
+          direction = 'upward'
           move()
         }
         break
@@ -206,7 +324,7 @@ document.addEventListener('DOMContentLoaded', () => {
         keyVal = 39
         if (playerIndex % width < width - 1) {
           playerIndex++
-
+          playerMove = playerMove + 1
           direction = 'forward'
           move()
         }
@@ -217,7 +335,8 @@ document.addEventListener('DOMContentLoaded', () => {
         keyVal = 40
         if (playerIndex + width < width * width) {
           playerIndex += width
-
+          playerMove = playerMove - width
+          direction = 'downward'
           move()
         }
         break
